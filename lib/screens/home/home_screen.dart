5330aa94fc204ee1ebc4ffefcb65a4079578d7b2
munshyn <alvin6565@gmail.com>
2022-05-16 01:59:30 +0800

    60% sign up
    Co-authored-by: amirnajwan <amirnajwan@users.noreply.github.com>

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:foodpanzu/components/coustom_bottom_nav_bar.dart';
-import 'package:foodpanzu/enums.dart';
+import 'package:foodpanzu/components/default_button.dart';
+import 'package:foodpanzu/screens/logout_success/logout_success_screen.dart';
+import 'package:foodpanzu/size_config.dart';
+import 'package:map_mvvm/map_mvvm.dart';
+// import 'package:foodpanzu/components/coustom_bottom_nav_bar.dart';
+// import 'package:foodpanzu/enums.dart';
 
 import 'components/body.dart';
+import 'home_viewmodel.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
+      appBar: AppBar(
+        title: Text(
+          'home',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // SearchField(),
+            View<HomeViewModel>(
+              builder: (_, viewmodel) => DefaultButton(
+                text: "Sign out",
+                press: () {
+                  viewmodel.signOut();
+                  // if all are valid then go to success screen
+                  Navigator.pushNamed(context, LogOutSuccessScreen.routeName);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      // Body(),
       // bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
     );
   }
