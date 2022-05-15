d1f9560c995646f0b24bdde56093fe3e3d625247
Mfikrilamin <muhd.fikrilamin@gmail.com>
2022-05-16 01:57:14 +0800

    80% Forgot password module

--- lib/screens/forgot_password/components/body.dart
+++ lib/screens/forgot_password/components/body.dart
@@ -4,11 +4,13 @@ import 'package:foodpanzu/components/default_button.dart';
 import 'package:foodpanzu/components/form_error.dart';
 import 'package:foodpanzu/components/no_account_text.dart';
 import 'package:foodpanzu/screens/forgot_password/forgot_password_viewmodel.dart';
+import 'package:foodpanzu/screens/sign_in/components/sign_form.dart';
 import 'package:foodpanzu/size_config.dart';
 import 'package:map_mvvm/map_mvvm.dart';
 // import 'package:foodpanzu/services/firebase_forget_password.dart';
 
 import '../../../constants.dart';
+import '../../sign_in/sign_in_screen.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -52,7 +54,6 @@ class ForgotPassForm extends StatefulWidget {
 class _ForgotPassFormState extends State<ForgotPassForm> {
   final _formKey = GlobalKey<FormState>();
   List<String> errors = [];
-  String? email;
 
   //forgot password model
   var forgotPassModel = forgotPasswordModel();
@@ -61,74 +62,73 @@ class _ForgotPassFormState extends State<ForgotPassForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (newValue) => email = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-                setState(() {
-                  errors.remove(kEmailNullError);
-                });
-              } else if (emailValidatorRegExp.hasMatch(value) &&
-                  errors.contains(kInvalidEmailError)) {
-                setState(() {
-                  errors.remove(kInvalidEmailError);
-                });
-              }
-            },
-            validator: (value) {
-              if (value!.isEmpty && !errors.contains(kEmailNullError)) {
-                setState(() {
-                  errors.add(kEmailNullError);
-                });
-              } else if (!emailValidatorRegExp.hasMatch(value) &&
-                  !errors.contains(kInvalidEmailError)) {
-                setState(() {
-                  errors.add(kInvalidEmailError);
-                });
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              labelText: "Email",
-              hintText: "Enter your email",
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
-            ),
-          ),
-          SizedBox(height: getProportionateScreenHeight(30)),
-          FormError(errors: errors),
-          SizedBox(height: SizeConfig.screenHeight * 0.1),
-          View<forgotPasswordModel>(
-            builder: (_, viewmodel) => DefaultButton(
-              text: "Continue",
-              press: () {
-                if (_formKey.currentState!.validate()) {
-                  //put a buffer loading while service is executed
-                  showDialog(
-                    context: context,
-                    barrierDismissible: false,
-                    builder: (context) =>
-                        Center(child: CircularProgressIndicator()),
-                  );
-
-                  //call password for get service
-
-                  // var message =
-                  //     ('${forgotPassModel.forgotPasswordUsingEmail(email)}');
-
-                  //show success message
-                  //show error message
-                  // SnackBar(content: Text(message));
+      child: View<forgotPasswordModel>(
+        builder: (_, viewModel) => Column(
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              onSaved: (newValue) => viewModel.email = newValue!,
+              onChanged: (value) {
+                if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+                  setState(() {
+                    errors.remove(kEmailNullError);
+                  });
+                } else if (emailValidatorRegExp.hasMatch(value) &&
+                    errors.contains(kInvalidEmailError)) {
+                  setState(() {
+                    errors.remove(kInvalidEmailError);
+                  });
                 }
+                viewModel.email = value;
               },
+              validator: (value) {
+                if (value!.isEmpty && !errors.contains(kEmailNullError)) {
+                  setState(() {
+                    errors.add(kEmailNullError);
+                  });
+                } else if (!emailValidatorRegExp.hasMatch(value) &&
+                    !errors.contains(kInvalidEmailError)) {
+                  setState(() {
+                    errors.add(kInvalidEmailError);
+                  });
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "Enter your email",
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+              ),
             ),
-          ),
-          SizedBox(height: SizeConfig.screenHeight * 0.1),
-          NoAccountText(),
-        ],
+            SizedBox(height: getProportionateScreenHeight(30)),
+            FormError(errors: errors),
+            SizedBox(height: SizeConfig.screenHeight * 0.1),
+            DefaultButton(
+                text: "Continue",
+                press: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    //put a buffer loading while service is executed
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) =>
+                          const Center(child: CircularProgressIndicator()),
+                    );
+                    viewModel.forgotPasswordUsingEmail(viewModel.email);
+
+                    var snackBar = SnackBar(
+                        content: Text(
+                            '${viewModel.hasFailure ? ('Error occured: \n' + (viewModel.failure?.toString())!) : viewModel.msg}'));
+                    Navigator.of(context).pop();
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                }),
+            SizedBox(height: SizeConfig.screenHeight * 0.1),
+            NoAccountText(),
+          ],
+        ),
       ),
     );
   }
