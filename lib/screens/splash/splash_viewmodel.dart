3d8407024046be64224d9d537a09c2856cafeb33
munshyn <alvin6565@gmail.com>
2022-06-06 16:24:25 +0800

    splash fix

--- lib/screens/splash/splash_viewmodel.dart
+++ lib/screens/splash/splash_viewmodel.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:foodpanzu/models/user_model.dart';
 import 'package:foodpanzu/app/service_locator.dart';
 import 'package:foodpanzu/screens/home/home_screen.dart';
+import 'package:foodpanzu/screens/restaurant_sign_up/restaurant_signup_screen.dart';
 import 'package:foodpanzu/services/firebase/firebase_service.dart';
 import 'package:map_mvvm/map_mvvm.dart';
 import '../owner_home/ownerhome_screen.dart';
@@ -15,6 +16,7 @@ class SplashViewModel extends Viewmodel {
   bool get isListeningToStream => _streamListener != null;
   late UserModel _user;
   String role = '';
+  String restId = '';
 
   @override
   void init() async {
@@ -38,12 +40,17 @@ class SplashViewModel extends Viewmodel {
   void goToHomePageBasedOnRole(BuildContext context) async {
     _user = await service.getUser(service.getCurrentUser()!.uid);
     role = _user.role;
+    restId = _user.restId;
 
     print(role);
 
     if (role == "owner") {
-      //return a owner home page
-      Navigator.pushNamed(context, OwnerHomeScreen.routeName);
+      if(restId == ""){
+        Navigator.pushNamed(context, RestaurantSignUpScreen.routeName);
+      } else{
+        //return a owner home page
+        Navigator.pushNamed(context, OwnerHomeScreen.routeName);
+      }
     } else {      
       //return a customer home page
       Navigator.pushNamed(context, HomeScreen.routeName);
